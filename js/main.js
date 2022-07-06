4a14584b04dc343df00e9d886ffe1bc23d26d640
srikanthrebi <srikanthrebi>
2022-07-06 08:00:54 +0100

    add.

---
+++ js/main.js
@@ -0,0 +1,107 @@
+function startAD(){
+    setTimeout(function(){
+        document.getElementById("road-1").style.transition='all 1s ease';
+
+
+    },250)
+    setTimeout(function(){
+
+    document.getElementById("icons1").style. transform= 'translate(0, 0) scale(1)';
+    document.getElementById("icons1").style.transition='all 1s ease';
+    document.getElementById("icons2").style.transform='translate(0, 0) scale(1)';
+    document.getElementById("icons2").style.opacity='1';
+    document.getElementById("deer").style.transform= 'translateY(0) scale(1)';
+    
+
+},350)
+
+    setTimeout(function(){
+        document.getElementById("road-2").style.transition='all 1s ease';
+        document.getElementById("road-2").style.opacity='1';
+       
+      
+        
+    },900)
+    setTimeout(function(){
+        document.getElementById("man").style.transform= 'translate(90px,30px) scale(1)';
+        document.getElementById("tracker-2").style.transform='scale(1)';
+        document.getElementById("tracker-1").style.opacity='0';
+        document.getElementById("deer").style.transform= 'translate(-80px,35px) scale(1)';
+        document.getElementById("tracker-1").style.transform='scale(1)';
+    },1100)
+    setTimeout(function(){
+        document.getElementById("tracker-2").style.opacity='0';
+        document.getElementById("icons1").style. transform= 'translate(138px, 201px) scale(0)';
+        document.getElementById("icons1").style.transition='all 2s linear';
+        document.getElementById("icons2").style. transform= 'translate(-1px, 223px) scale(0)';
+        document.getElementById("icons2").style.transition='all 2s linear';
+    },2200)
+    setTimeout(function(){
+        document.getElementById("intel").style.transition='all 1s ease';
+        document.getElementById("intel").style.opacity='1';
+        document.getElementById("intel").style.transform= 'translate(0)';
+    },6000)
+    setTimeout(function(){
+        document.getElementById("together").style.transition='all 1s ease';
+        document.getElementById("together").style.opacity='1';
+        document.getElementById("together").style.transform= 'translate(0)';
+    },6200)
+    setTimeout(function(){
+        document.getElementById("altera").style.transition='all 1s ease';
+        document.getElementById("altera").style.opacity='1';
+        document.getElementById("altera").style.transform= 'translate(0)';
+    },6400)
+    setTimeout(function(){
+        document.getElementById("deliver").style.transition='all 1s ease';
+        document.getElementById("deliver").style.opacity='1';
+        document.getElementById("deliver").style.transform= 'translate(0)';
+    },6600)
+    setTimeout(function(){
+        document.getElementById("tech").style.transition='all 1s ease';
+        document.getElementById("tech").style.opacity='1';
+        document.getElementById("tech").style.transform= 'translate(0)';
+    },6800)
+    setTimeout(function(){
+        document.getElementById("cars").style.transition='all 1s ease';
+        document.getElementById("cars").style.opacity='1';
+        document.getElementById("cars").style.transform= 'translate(0)';
+    },7000)
+        setTimeout(function(){
+        document.getElementById("smart").style.transition='all 1s ease';
+        document.getElementById("smart").style.opacity='1';
+        document.getElementById("smart").style.transform= 'translate(0)';
+    },7200)
+    setTimeout(function(){
+        document.getElementById("blue-color").style.transition='all 1s ease';
+        document.getElementById("blue-color").style.opacity='1';
+        document.getElementById("blue-color").style.top= '0';
+        
+    },7800)
+    setTimeout(function(){
+        
+        document.getElementById("inside").style.transition='all 1s ease';
+        document.getElementById("inside").style.opacity='1';
+        document.getElementById("inside").style.transform= 'translate(0)';
+    },8000)
+    setTimeout(function(){
+        document.getElementById("program").style.transition='all 1s ease';
+        document.getElementById("program").style.opacity='1';
+        document.getElementById("program").style.transform= 'translate(0)';
+    },8200)
+    setTimeout(function(){
+        document.getElementById("success").style.transition='all 1s ease';
+        document.getElementById("success").style.opacity='1';
+        document.getElementById("success").style.transform= 'translate(0)';
+    },8400)
+    setTimeout(function(){
+        document.getElementById("outside").style.transition='all 1s ease';
+        document.getElementById("outside").style.opacity='1';
+        document.getElementById("outside").style.transform= 'translate(0)';
+    },8600)
+    setTimeout(function(){
+        document.getElementById("link").style.transition='all 1s ease';
+        document.getElementById("link").style.opacity='1';
+        document.getElementById("link").style.transform= 'translate(0)';
+    },8800)
+    
+}window.load=startAD();
